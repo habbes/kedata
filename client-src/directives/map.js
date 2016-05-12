ca99ebd2cb7ee6a3a7e6659b63a1d0e9ba996aa2
Habbes <haby_habbes@outlook.com>
2016-05-13 02:12:19 +0300

    Add data vizualization on map

--- client-src/directives/map.js
+++ client-src/directives/map.js
@@ -4,39 +4,60 @@ import $ from 'jquery';
 export default function(){
     return {
         restrict: 'AE',
+        scope: {
+            data: '=db',
+            dataset: '=dataset'  
+        },
         link: function(scope, element, attrs) {
-           
-            console.log($(element));
-            // $(element).width(500);
-            $(element).height(700);
-            console.log('here');
-            
-            $(element).vectorMap
-            ({
-                map: 'kenya',
-                backgroundColor: 'transparent',
-                regionStyle: {
-                    initial: {
-                        fill: 'white',
-                        "fill-opacity": 1,
-                        stroke: '#aaa',
-                        "stroke-width": 2,
-                        "stroke-opacity": 1
-                    },
-                    hover: {
-                        "fill-opacity": 0.8,
-                        cursor: 'pointer'
-                    },
-                    selected: {
-                        fill: 'yellow'
-                    },
-                    selectedHover: {
-                    }
-                },
-                zoomOnScroll: false, // set it to false to avoid errors for missing mousewheel event
+            // set map height to prevent small map
+            $(element).height(700);            
+            scope.$watch('dataset', (dataset) => {
+                if(dataset){                    
+                    setTimeout(() => {
+                        drawMap(scope.data.data[dataset]);
+                    }, 20);
+                } 
             });
             
-            let map = $(element).vectorMap('get', 'mapObject');
+            /**
+             * deletes current map and draws a new one
+             * based on the specified dataset
+             */
+            function drawMap(dataset){
+                // first delete current map if any
+                $(element).html("");
+                $(element).vectorMap
+                ({
+                    map: 'kenya',
+                    backgroundColor: 'transparent',
+                    regionStyle: {
+                        initial: {
+                            fill: 'white',
+                            "fill-opacity": 1,
+                            stroke: '#aaa',
+                            "stroke-width": 2,
+                            "stroke-opacity": 1
+                        },
+                        hover: {
+                            "fill-opacity": 0.8,
+                            cursor: 'pointer'
+                        },
+                        selected: {
+                            fill: 'yellow'
+                        },
+                        selectedHover: {
+                        }
+                    },
+                    series: {
+                        regions: [{
+                            values: dataset,
+                            scale: ['#C8EEFF', '#0071A4'],
+                            normalizeFunction: 'polynomial'
+                        }]
+                    },
+                    zoomOnScroll: false, // set it to false to avoid errors for missing mousewheel event
+                });
+            }
         }
     };
 }
